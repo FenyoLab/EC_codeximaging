0af41b8300a4e928598f1e099d6527a7d38b4714
alizasiegman <alizasiegman@gmail.com>
2024-08-28 12:01:20 -0400

    adding new script

--- bash_scripts/main_label_images.sh
+++ bash_scripts/main_label_images.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=create_label_images
 #SBATCH --mail-type=FAIL # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --ntasks=1
-#SBATCH --output=../logs/main_label_images_sh.txt
-#SBATCH --error=../logs/main_label_images_sh.txt
+#SBATCH --output=../logs/main_label_images_%j.txt
+#SBATCH --error=../logs/main_label_images_err_%j.txt
 #SBATCH --mem=50G
 #SBATCH --time=4:00:00
 #SBATCH --cpus-per-task=1
