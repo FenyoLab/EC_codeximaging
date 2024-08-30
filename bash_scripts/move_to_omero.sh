a3742e6a21ca556f54a76559eabdc4a74ace84d5
alizasiegman <alizasiegman@gmail.com>
2024-08-30 13:25:42 -0400

    error fixes

--- bash_scripts/move_to_omero.sh
+++ bash_scripts/move_to_omero.sh
@@ -5,12 +5,12 @@
 #SBATCH --output=../logs/move_to_research_drive_%j.txt
 #SBATCH --error=../logs/move_to_research_drive_err_%j.txt
 #SBATCH --mem=2G
-#SBATCH --time=4:00:00
+#SBATCH --time=2:00:00
 #SBATCH --partition=data_mover
 
 source ~/.bashrc
 conda activate omero
 
-export YOUR_PASSWORD='535b9cpQ!'
+export YOUR_PASSWORD='your_password'
 cd /gpfs/home/as18894/projects/as18894/FenyoLab/Endometrial/EC_codeximaging/src/omero_analysis
 python move_to_omero.py
