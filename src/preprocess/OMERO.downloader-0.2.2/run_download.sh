e9a924774b2ef418ea6f54f1dd5cbe192f1eec7e
mhollenberg <mh6486@nyu.edu>
2024-10-01 14:16:32 -0400

    run_download change

--- src/preprocess/OMERO.downloader-0.2.2/run_download.sh
+++ src/preprocess/OMERO.downloader-0.2.2/run_download.sh
@@ -12,7 +12,7 @@
 for variable in 544833 544800 544812 544830 657369 544818 544821 544806 544797 544803 544827 544794 657363 657366 544809 544815 544776 544791 544788 544782 544779
 do 
         echo $variable
-	./download.sh -b '/gpfs/data/proteomics/data/Endometrial_mIF/SVS_HE' -s omero.nyumc.org -u mh6486 -w 'Snowy123!!!!!' -f binary Image:$variable
+	./download.sh -b '/gpfs/data/proteomics/data/Endometrial_mIF/SVS_HE' -s omero.nyumc.org -u mh6486 -w 'PASSWORD' -f binary Image:$variable
 done 
 
 
