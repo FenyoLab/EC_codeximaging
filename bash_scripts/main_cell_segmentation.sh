278d0c9be1b0bafc6873a783c26f534fe9544467
alizasiegman <alizasiegman@gmail.com>
2024-11-08 08:28:16 -0500

    removed error log and changed partition

--- bash_scripts/main_cell_segmentation.sh
+++ bash_scripts/main_cell_segmentation.sh
@@ -3,11 +3,10 @@
 #SBATCH --mail-type=FAIL # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --ntasks=1
 #SBATCH --output=../logs/cell_segmentation_%j.txt
-#SBATCH --error=../logs/cell_segmentation_err_%j.txt
 #SBATCH --mem=200G
 #SBATCH --time=72:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=cpu_medium
+#SBATCH --partition=fn_medium
 
 module load condaenvs/new/deepcell
 
