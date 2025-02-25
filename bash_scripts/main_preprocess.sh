debe78e15017664270176a7c05799a2ea6fcf2ee
Areff <ca3261@bigpurple-ln3.cm.cluster>
2025-02-25 15:12:13 -0500

    update preprocessing for cervical cancer

--- bash_scripts/main_preprocess.sh
+++ bash_scripts/main_preprocess.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=preprocessing
-#SBATCH --mail-type=FAIL # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --ntasks=1
 #SBATCH --output=../logs/preprocessing_%j.txt
-#SBATCH --mem=600G
-#SBATCH --time=24:00:00   
-#SBATCH --partition=fn_medium
-source ~/.bashrc
-conda activate canvas 
+#SBATCH --partition gpu4_short,gpu8_short,gpu4_dev,gpu8_dev,gpu4_medium,gpu8_medium,gpu4_long,gpu8_long
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=300GB
+#SBATCH --time=4:00:00
+#SBATCH --gres=gpu:4
 
-cd ../
-python run_preprocess.py 
+# source ~/.bashrc
+source /gpfs/share/apps/miniconda3/gpu/4.9.2/etc/profile.d/conda.sh
+conda activate /gpfs/data/proteomics/projects/miniconda3/envs/canvas
+conda info
+
+pwd
+cd ..
+python run_preprocess.py
