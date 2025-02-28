2cd6f6feeca350f65d34ed0b78884ecaef4866c1
lp2700 <lp2700@nyu.edu>
2025-02-28 14:26:23 -0500

    image registration changes

--- src/preprocess/registration/main_register.py
+++ src/preprocess/registration/main_register.py
@@ -4,19 +4,15 @@ import pandas as pd
 import json
 from valis import registration
 import pdb
-#import geojson
 
 import csv_to_geojson, geojson_to_csv
 import registration
 
-image_dir = '../data/images'
-annotations_dir = '../data/annotations'
-
 #sample = sample_qptiff name 
 
-for sample in os.listdir(image_dir):
-    if sample != '20231019-4475-1E_Scan2': # HAVE TO RUN THIS ONE SAMPLE AT A TIME
-        continue
+def run_registration(sample):
+    image_dir = '../data/images'
+    annotations_dir = '../data/annotations'
 
     print(f'Processing sample {sample}')
     sample_path = os.path.join(image_dir, sample)
@@ -30,7 +26,7 @@ for sample in os.listdir(image_dir):
         elif ext == '.svs':
             sample_svs_name = file_name  # Without .svs
             print("sample svs name:", sample_svs_name)
-
+    pdb.set_trace()
     transformed_geojson_file = os.path.join(annotations_dir, sample, f'{sample_qptiff_name}_rois.geojson')
     if os.path.exists(transformed_geojson_file):
         print(f"Transformed rois geojson already exists, skipping sample {sample}")
@@ -44,7 +40,5 @@ for sample in os.listdir(image_dir):
     # Perform registration and transfer annotations
     registration.convert_rois_with_registration(sample, sample_svs_name, sample_qptiff_name)
 
-    # Convert the GeoJSON file back to CSV
-    #transformed_csv_file = os.path.join(annotations_dir, sample, f'{sample_qptiff_name}_rois.csv')
-    #geojson_to_csv.geojson_to_csv(transformed_geojson_file, transformed_csv_file)
+    exit()
 
