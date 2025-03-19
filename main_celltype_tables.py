f423fcbfc05c78e74fb6e63e400a0eedbf1bb2ee
alizasiegman <alizasiegman@gmail.com>
2025-03-19 16:01:44 -0400

    updated main scripts (match the new src code)

--- main_celltype_tables.py
+++ main_celltype_tables.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+from src.omero_tables import gen_omero_tables, upload_omero_table
+
 from types import SimpleNamespace
 from utils import helper
 
@@ -8,20 +10,18 @@ config_yaml= 'config/config_cellsegmentation.yaml'
 run_config = helper.load_yaml_file(config_yaml)
 config = SimpleNamespace(**run_config)
 
-from src.omero_tables import celltype_tables, upload_omero_table
-
-#create celltype cluster tables for omero
+#define relevant paths
 cell_types_path = os.path.join(config.out_dir, config.celltypes_dir, f'{config.n_clusters_celltypes}_clusters')
+sample_names_path = os.path.join(config.segmentation_data_dir, 'cell_sample_names.npy')
 omero_table_dir = os.path.join(config.out_dir, config.omero_table_dir)
-
 out_suffix = os.path.basename(config.out_dir)
 date = "_".join(out_suffix.split("_")[1:])
-table_name_celltypes = f'celltypes_{date}'
-print('Table name:', table_name_celltypes)
 
-celltype_tables.celltype_tables(cell_types_path = cell_types_path, save_path = omero_table_dir, 
-                                omero_dict = config.omero_image_dict, n_clusters = config.n_clusters_celltypes,
-                                table_name = table_name_celltypes, samples_to_remove=config.samples_to_remove)
+#create celltype cluster tables for omero
+table_name_celltypes = f'celltypes_{date}'
+gen_omero_tables.celltype_tables(sample_names_path = sample_names_path, cell_types_path = cell_types_path, 
+                            omero_table_path = omero_table_dir, omero_dict = config.omero_image_dict, 
+                            table_name = table_name_celltypes, n_clusters = config.n_clusters_celltypes)
 
 #upload celltype cluster tables to omero
 upload_omero_table.upload_omero_table(table_dir = omero_table_dir, table_name = table_name_celltypes, 
